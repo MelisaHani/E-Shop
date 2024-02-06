cc8d777eb0b2d81c440b30d65858676e9aa2fec1
MelisaHani <80726417+MelisaHani@users.noreply.github.com>
2024-02-06 21:57:29 +0100

    Update init.sql

--- init-scripts/init.sql
+++ init-scripts/init.sql
@@ -1,6 +1,7 @@
 INSERT INTO manufacturers (id,name, manufacturer_address) VALUES
 ('Nike Inc.', 'Nike Address'),
 ('Adidas Inc.', 'Adidas Address'),
+('Samsung', 'Samsung Addr'),
 ('Apple', 'Apple LA');
 
 INSERT INTO categories (id,name, description) VALUES
